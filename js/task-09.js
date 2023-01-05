908dc0366b7112fc4fff3f7ad7a468c6ea23b202
Oleksandr1301 <viss82@ukr.net>
2023-01-05 15:34:02 +0200

    add task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorSpanEl = document.querySelector('.color');
+const buttonEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+
+buttonEl.addEventListener('click', changeColorEl);
+
+function changeColorEl() {
+  let randomHexColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomHexColor;
+  colorSpanEl.textContent = randomHexColor;
+}
