934dc25f6e6402411ba72b4893b5666e3b83b491
Oleksandr1301 <viss82@ukr.net>
2023-01-05 15:07:59 +0200

    add task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector("form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("All fields must be completed!");
+  }
+
+  console.log({ Email: email.value, Password: password.value });
+
+  event.currentTarget.reset();
+}
